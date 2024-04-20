ed67968bead846242ec80eabc6de2439075c9d5f
ahmedmansour779 <ahmedmansour1239@gmail.com>
2024-04-20 20:01:31 +0200

    slider ourClient

--- src/modules/Home/OurClient/OurClient.jsx
+++ src/modules/Home/OurClient/OurClient.jsx
@@ -1,6 +1,11 @@
-import { useSelector } from "react-redux"
-import { ourClientData } from "../../../data/ourClient"
+import { useSelector } from "react-redux";
+import { ourClientData } from "../../../data/ourClient";
+// Import Swiper React components
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
 export default function OurClient() {
     const { ourClients } = useSelector((state) => state.translations.translations)
     return (
@@ -8,15 +13,42 @@ export default function OurClient() {
             <h1 className="text-4xl font-medium">
                 {ourClients}
             </h1>
-            <div className="flex gap-4 md:flex-row flex-col">
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={2}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 40,
+                    },
+                    1024: {
+                        slidesPerView: 5,
+                        spaceBetween: 50,
+                    },
+                }}
+                loop
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+
+                className='md:w-1/2 w-full  overflow-hidden'
+            >
                 {
                     ourClientData.map((item, index) => {
                         return (
-                            <img className="w-fit" src={item} key={index} />
+                            <SwiperSlide key={index} className='w-fit'>
+                                <img className="w-fit" src={item} />
+                            </SwiperSlide>
                         )
                     })
                 }
-            </div>
+            </Swiper>
         </div>
     )
 }
